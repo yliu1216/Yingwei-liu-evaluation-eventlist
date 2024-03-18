437858c7e2301b8dbbd183de78509d13012390af
liujia1 <lyw1532508619@outlook.com>
2024-03-18 02:15:39 -0400

    second commit

--- index.js
+++ index.js
@@ -31,8 +31,15 @@ async function fetchEvents(){
             <td>${value.startDate}</td>
             <td>${value.endDate}</td>
             <td>
-              <button class="editBtn" id=${value.id}>Edit</button>
-              <button class="deleteBtn" id=${value.id}>Delete</button>
+            <button class="editBtn" id="${value.id}">
+            <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small">
+              <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path>
+            </svg>
+            Edit
+          </button>
+              <button class="deleteBtn" id=${value.id}>
+              <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
+              Delete</button>
             </td>
             `;
             tableBody.appendChild(row);
@@ -104,8 +111,14 @@ addButton.addEventListener('click', function(e){
         <td><input class="startDate" type="date"/></td>
         <td><input class="endDate" type="date" /></td>
         <td>
-        <button class="addBtn">Add</button>
-        <button class="cancelBtn">Cancel</button>
+        <button class="addBtn">
+        <svg focusable viewBox="0 0 24 24" aria-hidden="true xmlns="http://www.w3.org/2000/svg"><path d="M12 6V18M18 12H6" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg>
+        Add
+        </button>
+        <button class="cancelBtn">
+        <svg focusable="false" aria-hidden="true" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M19.587 16.001l6.096 6.096c0.396 0.396 0.396 1.039 0 1.435l-2.151 2.151c-0.396 0.396-1.038 0.396-1.435 0l-6.097-6.096-6.097 6.096c-0.396 0.396-1.038 0.396-1.434 0l-2.152-2.151c-0.396-0.396-0.396-1.038 0-1.435l6.097-6.096-6.097-6.097c-0.396-0.396-0.396-1.039 0-1.435l2.153-2.151c0.396-0.396 1.038-0.396 1.434 0l6.096 6.097 6.097-6.097c0.396-0.396 1.038-0.396 1.435 0l2.151 2.152c0.396 0.396 0.396 1.038 0 1.435l-6.096 6.096z"></path></svg>
+        Cancel
+        </button>
         </td>
     `;
     tableBody.appendChild(row);
@@ -154,8 +167,9 @@ eventLists.addEventListener('click', (e) => {
         e.preventDefault();
         const id = e.target.id;
         console.log(id);
+    
         const row = e.target.parentElement.parentElement;
-
+        console.log(row);
         // Retrieve the current values
         const eventName = row.querySelector('td:nth-child(1)').textContent;
         const startDate = row.querySelector('td:nth-child(2)').textContent;
@@ -167,8 +181,8 @@ eventLists.addEventListener('click', (e) => {
             <td><input class="startDate" type="date" value="${startDate}" /></td>
             <td><input class="endDate" type="date" value="${endDate}" /></td>
             <td>
-                <button class="saveBtn">Save</button>
-                <button class="cancelBtn">Cancel</button>
+                <button class="saveBtn"><svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21,20V8.414a1,1,0,0,0-.293-.707L16.293,3.293A1,1,0,0,0,15.586,3H4A1,1,0,0,0,3,4V20a1,1,0,0,0,1,1H20A1,1,0,0,0,21,20ZM9,8h4a1,1,0,0,1,0,2H9A1,1,0,0,1,9,8Zm7,11H8V15a1,1,0,0,1,1-1h6a1,1,0,0,1,1,1Z"/></svg>Save</button>
+                <button class="cancelBtn"><svg focusable="false" aria-hidden="true" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M19.587 16.001l6.096 6.096c0.396 0.396 0.396 1.039 0 1.435l-2.151 2.151c-0.396 0.396-1.038 0.396-1.435 0l-6.097-6.096-6.097 6.096c-0.396 0.396-1.038 0.396-1.434 0l-2.152-2.151c-0.396-0.396-0.396-1.038 0-1.435l6.097-6.096-6.097-6.097c-0.396-0.396-0.396-1.039 0-1.435l2.153-2.151c0.396-0.396 1.038-0.396 1.434 0l6.096 6.097 6.097-6.097c0.396-0.396 1.038-0.396 1.435 0l2.151 2.152c0.396 0.396 0.396 1.038 0 1.435l-6.096 6.096z"></path></svg>Cancel</button>
             </td>
         `;
 
